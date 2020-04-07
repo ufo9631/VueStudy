9fe4c0188f002eb5f604dfb6c4bc0c107fb840d1
编程猴子无大脑 <695901733@qq.com>
2020-04-07 22:49:44 +0800

    webpack

--- webpack-study/src/main.js
+++ webpack-study/src/main.js
@@ -6,8 +6,17 @@
 import $ from 'jquery'
 
 $(function(){
-    $('li:odd').css("backgroundColor",'lightblue');
+    $('li:odd').css("backgroundColor",'black');
     $('li:even').css("backgroundColor",function(){
         return '#'+"D97634";
     });
 })
+
+//使用webpack-dev-server 这个工具 来实现自动打包编译的功能
+// 1. 运行npm i webpack-dev-server -D 把这个工具安装到项目的本地开发依赖
+// 2.安装完毕后，这个工具的用法和webpack命令的用法 完全一样
+// 3.由于我们是在项目中安装的 所以无法把它当做脚本命令执行
+
+// 4.注意 webpack-dev-server 这个工具 如果想要正常运行，要求在本项目中必须安装webpack(就算全局安装过也需要在本地安装一次)      npm i webpack -D
+// 5.webpack-dev-server 帮我们打包生成的bundle.js文件并没有存放到实际的物理磁盘上，而是直接托管到电脑的内存中，所以我们再项目根目录中，根本找不到这个包打好的bundle.js
+// 6.我们可以认为webpack-dev-server把打包好的文件，以一种虚拟的形式，托管到我们的项目中
--- webpack-study/webpack.config.js
+++ webpack-study/webpack.config.js
@@ -1,11 +1,23 @@
 const path=require('path')
+const webpack=require('webpack') //启用热更新的第二部
 //这个配置文件，通过node中的模块操作，向外暴露一个配置对象
 module.exports={
  entry:path.join(__dirname,'/src/main.js'), //表示webpack打包哪个文件
  output:{
      path:path.join(__dirname,'/dist'), //指定打包好的文件输出到哪里去
      filename:"bundle.js" //指定输出的文件名称
- }
+ },
+ devServer:{//这是配置dev-server命令参数的第二种形式，相对来说比较麻烦
+    open:true,//自动打开浏览器
+    port:300,//设置启动的端口
+    contentBase:'src',//指定托管的根目录
+    hot:true //启用热更新的第一步
+
+ },
+ plugins:[//配置插件的节点
+   new  webpack.HotModuleReplacementPlugin() //new 一个热更新的模块对象  。这是启用热更新的第三步
+
+ ]
 }
 
 //当我们再webpack控制直接输入webpack命令执行的时候，webpack做了以下步骤
